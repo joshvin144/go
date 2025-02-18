57fc32acf72ed32bbee4dd7b299dfd70fbe2b929
joshvin144 <joshvin144@gmail.com>
2025-02-17 19:01:28 -0800

    add: add config file (TOML)
    1. add single_include/toml.hpp
    2. add config file
    3. read config file for game board dimension

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
  * -- Stones
  */
 
-// /usr/bin/g++ -std=c++14 -lcurses -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/GameBoard/ -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/Players/ -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/GameManager/ -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/GameManager/ASCII/ -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/KeyboardMonitor/ -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/DisplayManager/ -fdiagnostics-color=always -g /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/*.cpp /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/GameBoard/*.cpp /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/Players/*.cpp /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/GameManager/*.cpp /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/GameManager/ASCII/*.cpp /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/KeyboardMonitor/*.cpp /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/DisplayManager/*.cpp -o /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/main
+// /usr/bin/g++ -std=c++14 -lcurses -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/single_include/ -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/GameBoard/ -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/Players/ -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/GameManager/ -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/GameManager/ASCII/ -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/KeyboardMonitor/ -I /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/DisplayManager/ -fdiagnostics-color=always -g /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/*.cpp /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/GameBoard/*.cpp /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/Players/*.cpp /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/GameManager/*.cpp /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/GameManager/ASCII/*.cpp /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/KeyboardMonitor/*.cpp /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/DisplayManager/*.cpp -o /Users/jvincent/Documents/ProfessionalDevelopment/ContinuousLearning/Projects/Repositories/go/main
 
 #include <iostream>
 #include <stdint.h>
@@ -19,44 +19,16 @@
 #include "game_manager.h"
 #include "keyboard_monitor.h"
 #include "display_manager.h"
+#include "toml.hpp"
 
 
-#define DIFFICULTY_LEVEL_LOW          (0u)
-#define DIFFICULTY_LEVEL_INTERMEDIATE (1u)
-#define DIFFICULTY_LEVEL_HARD         (2u)
-
-#define DIFFICULTY_LEVEL_UNRECOGNIZED_GRID (1u)
-#define DIFFICULTY_LEVEL_LOW_GRID          (9u)
-#define DIFFICULTY_LEVEL_INTERMEDIATE_GRID (13u)
-#define DIFFICULTY_LEVEL_HARD_GRID         (19u)
+#define CONFIG_PATH ("./config.toml")
 
 
 int main(int argc, char* argv[])
 {
-    uint32_t u32_grid_option = 0;
-    uint8_t u8_grid_dimension = 0;
-    std::cout << "Grid dimension:\n0: 9x9\n1:13x13\n2:19x19\n";
-    std::cin >> u32_grid_option;
-
-    switch(u32_grid_option)
-    {
-        case DIFFICULTY_LEVEL_LOW:
-            std::cout << "Difficulty level low selected...\n";
-            u8_grid_dimension = DIFFICULTY_LEVEL_LOW_GRID;
-            break;
-        case DIFFICULTY_LEVEL_INTERMEDIATE:
-            std::cout << "Difficulty level itermediate selected...\n";
-            u8_grid_dimension = DIFFICULTY_LEVEL_INTERMEDIATE_GRID;
-            break;
-        case DIFFICULTY_LEVEL_HARD:
-            std::cout << "Difficulty level hard selected...\n";
-            u8_grid_dimension = DIFFICULTY_LEVEL_HARD_GRID;
-            break;
-        default:
-            std::cout << "Unrecognized difficulty level selected...\n";
-            u8_grid_dimension = DIFFICULTY_LEVEL_UNRECOGNIZED_GRID;
-    }
-    printf("Setting up %dx%d grid game board...\n", u8_grid_dimension, u8_grid_dimension);
+    // Load config
+    auto config = toml::parse(CONFIG_PATH);
 
     // Instantiate the necessary managers
     GameManager game_manager;
@@ -84,7 +56,7 @@ int main(int argc, char* argv[])
     game_manager.add_player(COMPUTER_PLAYER);
 
     // Set up a game of Go
-    game_manager.setup_board_game(&game_board, u8_grid_dimension + 2u);
+    game_manager.setup_board_game(&game_board, (uint8_t)config.at("grid_dimension").as_integer());
     // The Game Manager will update the game board and the Display Manager will display it
     // Therefore, the Display Manager must also be aware of the Game Board
     display_manager.register_game_board(&game_board);
